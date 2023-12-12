b59591018afab9a64a3fbc3b6491046bab17a04d
Derek Mercedes <mercedes.derek@gmail.com>
2023-12-11 18:52:06 -0700

    tests passed

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    count =1
+    for dwarf in dwarves:
+        print(f"{count}. {dwarf}")
+        count +=1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_Calls):
+     return [call.capitalize()+"!" for call in planeteer_Calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for cheese in cheeses:
+        if cheese in snacks:
+            return cheese
